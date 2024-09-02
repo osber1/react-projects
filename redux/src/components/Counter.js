c8aca4e9ab3b5d32365a8b29cab1041d50658287
Osvaldas Bernatavičius <osvaldas.bernatavicius@4finance.com>
2024-09-02 22:44:35 +0300

    feat(redux): implement Redux for state management
    Added Redux to manage the state of the counter application. Created a new Redux store with actions for incrementing, decrementing, increasing by a specific amount, and toggling the counter display. Updated the Counter component to dispatch these actions and select state from the Redux store.

--- redux/src/components/Counter.js
+++ redux/src/components/Counter.js
@@ -1,12 +1,36 @@
-import classes from './Counter.module.css';
+import classes from "./Counter.module.css";
+import { useDispatch, useSelector } from "react-redux";
 
 const Counter = () => {
-  const toggleCounterHandler = () => {};
+  const dispatch = useDispatch();
+  const counter = useSelector((state) => state.counter);
+  const show = useSelector((state) => state.showCounter);
+
+  const toggleCounterHandler = () => {
+    dispatch({ type: "TOGGLE" });
+  };
+
+  const incrementCounterHandler = () => {
+    dispatch({ type: "INCREMENT" });
+  };
+
+  const increaseCounterHandler = () => {
+    dispatch({ type: "INCREASE", amount: 5 });
+  };
+
+  const decrementCounterHandler = () => {
+    dispatch({ type: "DECREMENT" });
+  };
 
   return (
     <main className={classes.counter}>
       <h1>Redux Counter</h1>
-      <div className={classes.value}>-- COUNTER VALUE --</div>
+      {show && <div className={classes.value}>{counter}</div>}
+      <div>
+        <button onClick={decrementCounterHandler}>Decrement</button>
+        <button onClick={incrementCounterHandler}>Increment</button>
+        <button onClick={increaseCounterHandler}>Increase by 5</button>
+      </div>
       <button onClick={toggleCounterHandler}>Toggle Counter</button>
     </main>
   );
